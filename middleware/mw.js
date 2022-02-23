2ec22186b183b33712ec84d8f97495d1c4175260
Vighnesh Manjrekar <vighneshmanjrekar4193@gmail.com>
2022-02-24 00:58:16 +0530

    Add middleware code

---
+++ middleware/mw.js
@@ -0,0 +1,19 @@
+const util = require('util')
+const multer = require('multer')
+const { GridFsStorage } = require('multer-gridfs-storage')
+const { uploadFiles } = require('../controllers/home')
+
+const storage = new GridFsStorage({
+    url: "mongodb://localhost:27017/image_upload",
+    options: { useNewUrlParser: true, useUnifiedTopology: true },
+    file: (req, file) => {
+        return {
+            bucketName: 'photos',
+            filename: 'file_' + Date.now()
+        }
+    },
+})
+
+const uploadFiles = multer({ storage }).array("file", 10);
+const uploadFilesMiddleware = util.promisify(uploadFiles)
+module.exports = uploadFilesMiddleware
